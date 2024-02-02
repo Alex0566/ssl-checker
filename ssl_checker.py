aa6efb56cbc144af421ab18a58c866134da4c0bb
Super Devil <macbook_m3@MacBook-Pro-Super.local>
2024-02-02 19:18:30 +0300

    Добавлен файл ssl_checker.py

--- ssl_checker.py
+++ ssl_checker.py
@@ -1,27 +1,33 @@
 import ssl
 import socket
 import datetime
+from urllib.parse import urlparse
+from idna import encode
+
+def punycode_encode(domain):
+    return encode(domain).decode('utf-8')
 
 def get_certificate_expiry_date(hostname, port=443):
     context = ssl.create_default_context()
-    conn = context.wrap_socket(socket.create_connection((hostname, port)), server_hostname=hostname)
-    cert = conn.getpeercert()
-    conn.close()
-    cert_expiry_date = datetime.datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
-    days_until_expiry = (cert_expiry_date - datetime.datetime.utcnow()).days
-    return days_until_expiry
+    try:
+        conn = context.wrap_socket(socket.create_connection((hostname, port)), server_hostname=hostname)
+        cert = conn.getpeercert()
+        conn.close()
+        cert_expiry_date = datetime.datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
+        days_until_expiry = (cert_expiry_date - datetime.datetime.utcnow()).days
+        return days_until_expiry
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 def check_ssl_expiry(domains):
     results = []
 
     for domain in domains:
-        try:
-            days_until_expiry = get_certificate_expiry_date(domain)
-            results.append((domain, days_until_expiry))
-        except Exception as e:
-            results.append((domain, "Error: " + str(e)))
+        punycode_domain = punycode_encode(domain)
+        days_until_expiry = get_certificate_expiry_date(punycode_domain)
+        results.append((domain, days_until_expiry))
 
-    sorted_results = sorted(results, key=lambda x: x[1])
+    sorted_results = sorted(results, key=lambda x: (float('inf') if isinstance(x[1], str) else x[1], x[0]))
     return sorted_results
 
 if __name__ == "__main__":
@@ -29,7 +35,9 @@ if __name__ == "__main__":
         'sto-good.ru',
         'automotor-center.ru',
         'remont-turbin.info',
-        'sto-top.ru'
+        'sto-top.ru',
+        'домен-с-кириллицей.рф',
+        'мох.москва'
     ]
 
     sorted_results = check_ssl_expiry(domains)
